89de41874eabda84e716ed390a162966d32d137a
Nguyen Minh Anh <xiaosasori@gmail.com>
2024-08-31 18:26:13 +0700

    feat: basic show solutions feature

--- components/PanelEditor.vue
+++ components/PanelEditor.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { Pane, Splitpanes } from 'splitpanes'
+import ButtonShowSolution from './content/ButtonShowSolution.vue'
 import { filesToVirtualFsTree } from '~/templates/utils'
 
 const play = usePlaygroundStore()
@@ -22,7 +23,7 @@ watch(
 )
 
 watch(
-  () => play.mountedGuide,
+  () => [play.mountedGuide, play.showingSolution],
   () => {
     input.value = play.fileSelected?.read() || ''
   },
@@ -42,29 +43,36 @@ function endDragging(e: { size: number }[]) {
 }
 // For panes size initialization on SSR
 const isMounted = useMounted()
-const panelInitFileTree = computed(
-  () =>
-    isMounted.value || {
-      width: `${ui.panelFileTree}%`,
-    },
+// eslint-disable-next-line prettier/prettier
+const panelInitFileTree = computed(() => isMounted.value || {
+  width: `${ui.panelFileTree}%`,
+},
 )
-const panelInitEditor = computed(
-  () =>
-    isMounted.value || {
-      width: `${100 - ui.panelFileTree}%`,
-    },
+// eslint-disable-next-line prettier/prettier
+const panelInitEditor = computed(() => isMounted.value || {
+  width: `${100 - ui.panelFileTree}%`,
+},
 )
 </script>
 
 <template>
   <div class="grid h-full grid-rows-[min-content_1fr]">
     <div
-      class="border-base bg-faded flex items-center gap-2 border-b border-dashed px-4 py-2"
+      class="border-base bg-faded flex justify-between border-b border-dashed px-4 py-2"
     >
-      <Icon name="i-ph-text-t-duotone" />
-      <span class="text-sm">Editor</span>
+      <div class="flex items-center gap-2">
+        <Icon name="i-ph-text-t-duotone" />
+        <span class="text-sm">Editor</span>
+      </div>
+      <ButtonShowSolution
+        class="hover:bg-active -my-1 -mr-3 flex-none rounded px-2 py-1 text-sm opacity-50 hover:opacity-100"
+      />
     </div>
-    <Splitpanes @resize="startDragging" @resized="endDragging">
+    <Splitpanes
+      class="overflow-hidden"
+      @resize="startDragging"
+      @resized="endDragging"
+    >
       <Pane
         class="flex h-full flex-col overflow-auto"
         :size="ui.panelFileTree"
---
+++ components/content/ButtonShowSolution.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+const play = usePlaygroundStore()
+</script>
+
+<template>
+  <button
+    v-if="play.mountedGuide?.solutions"
+    class="flex items-center gap-2"
+    @click="play.mountGuide(play.mountedGuide, !play.showingSolution)"
+  >
+    <Icon v-if="!play.showingSolution" name="i-ph-lightbulb-filament-duotone" />
+    <Icon v-else name="i-ph-arrow-counter-clockwise-duotone" />
+    {{ play.showingSolution ? 'Reset challenge' : 'Show solution' }}
+  </button>
+</template>
